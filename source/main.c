f5cbb9f151ea2bcc983b9667d59eb8b5c4937618
Simen Allum <simensal@stud.ntnu.no>
2020-02-13 16:05:25 +0100

    Added main while loop

--- source/main.c
+++ source/main.c
@@ -28,6 +28,43 @@ int main(){
 
     fsm_init();
 
+    while(1){
+        if(hardware_read_stop_signal()){
+            fsm_ev_stopButton_pressed();
+        }
+
+        for (int i = 0; i < HARDWARE_NUMBER_OF_FLOORS; ++i){
+            if(hardware_read_floor_sensor(i)){
+                if (i != prev_floor){
+                    hardware_command_floor_indicator_on(i);
+                }
+                fsm_ev_reach_floor(i);
+            }
+        }
+
+        for (int floor = 0; floor < HARDWARE_NUMBER_OF_FLOORS; floor++){
+            for (int i = HARDWARE_ORDER_UP; i <= HARDWARE_ORDER_DOWN; i++){
+                if ((floor == 0) && (i == HARDWARE_ORDER_DOWN)){
+                    continue;
+                }
+                if ((floor == 3) && (i == HARDWARE_ORDER_UP)){
+                    continue;
+                }
+                fsm_ev_set_queue(floor, i);
+            }
+        }
+
+        if(check_timeout()){
+            fsm_ev_timeout();
+        }
+
+        if(hardware_read_obstruction_signal()){
+            fsm_ev_obstruction();
+        }
+        
+        fsm_ev_request();
+    }
+
     /**
     printf("=== Example Program ===\n");
     printf("Press the stop button on the elevator panel to exit\n");
