6ee0b4d86f62fdce19ed3ed25dcae5129e29bf0b
simenallum <simensal@stud.ntnu.no>
2020-02-25 09:23:46 +0100

    changed seconds -> m_seconds

--- source/timer.c
+++ source/timer.c
@@ -9,15 +9,15 @@
 #include "timer.h"
 #include "time.h"
 
-static time_t seconds = 0;
+static time_t m_seconds = 0;
 
 void timer_start() {
-    seconds = time(NULL);
+    m_seconds = time(NULL);
 }
 
 
 int timer_check_timeout() {
-    if (time(NULL) - seconds >= 3) {
+    if (time(NULL) - m_seconds >= 3) {
         return 1;
     }
     return 0;
